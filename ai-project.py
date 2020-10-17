68cc1d5c9f0504f55463a8d4c0994a2ad4d08bce
OhThomas <thomasdonnrader@yahoo.com>
2020-10-16 21:36:46 -0500

    Added comments and counted steps taken

--- ai-project.py
+++ ai-project.py
@@ -1,6 +1,7 @@
 import random
 from random import shuffle, randrange
 
+# Agent class that contains all the information needed for our agents
 class Agent:
     def __init__(self,name,top,bottom,left,right):
         self.name = name
@@ -8,12 +9,15 @@ class Agent:
         self.bottom = bottom
         self.right = right
         self.left = left
-width = 16
-height = 8
-#(*3 because "|  " and "+--"; +2 because \n and wall) use less space("+-") to make it *2
-widthLength = ((width*3)+2) # =50
-gameComplete = 0
 
+width = 16 # width of maze
+height = 8 # height of maze
+widthLength = ((width*3)+2) # length of the string width including \n and wall (50)
+#(*3 because "|  " and "+--"; +2 because \n and wall) use less space("+-") to make it *2
+gameComplete = 0 # used to loop the program until the game is complete
+stepsTaken = 0 # count how many steps taken until the seeker finds the hider
+
+# generates a maze
 def make_maze(w = width, h = height):
     vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
     ver = [["|  "] * w + ['|'] for _ in range(h)] + [[]]
@@ -37,6 +41,7 @@ def make_maze(w = width, h = height):
         s += ''.join(a + ['\n'] + b + ['\n'])
     return s
 
+# replaces the first wall "|" in the maze allowing for an entrance
 def make_entrance(maze):
     #print(maze[49:])
     global widthLength
@@ -45,6 +50,7 @@ def make_entrance(maze):
     maze = "".join(maze)
     return maze
 
+# adds agent to the first available space " " scanning from top to bottom, left to right
 def add_agent(maze,agent):
     mazeList = list(maze)
     i = 0
@@ -56,6 +62,7 @@ def add_agent(maze,agent):
     agent = whatIsAvailable(maze,agent)
     return maze
 
+# adds agent to a spot with a given location (be careful)
 def add_agent_spot(maze,agent,spot):
     mazeList = list(maze)
     mazeList[spot] = agent.name
@@ -79,12 +86,15 @@ def add_agent_random_spot(maze,agent):
     agent = whatIsAvailable(maze,agent)
     return maze
 
+# resets agents data
 def agent_reset(agent):
     agent.top = -1
     agent.bottom = -1
     agent.left = -1
     agent.right = -1
     return agent
+
+# checks around the seeker for the hider, if found gameComplete = 1 and the game is over
 def checkForHider(mazeList, agent,i):
     global gameComplete
     global widthLength
@@ -96,6 +106,8 @@ def checkForHider(mazeList, agent,i):
         gameComplete = 1
     if mazeList[i+widthLength] == "1":
         gameComplete = 1
+
+# looks around the agent for " " and updates it's data accordingly
 def whatIsAvailable(maze, agent):
     global width
     global widthLength
@@ -114,7 +126,9 @@ def whatIsAvailable(maze, agent):
     checkForHider(mazeList, agent,i)
     return agent
 
+# randomly traverses through the maze
 def randomTraverse(maze, agent):
+    global stepsTaken
     mazeList = list(maze)
     i = 0
     choices = 0
@@ -143,8 +157,10 @@ def randomTraverse(maze, agent):
     agent = agent_reset(agent)
     maze = "".join(mazeList)
     agent = whatIsAvailable(maze,agent)
+    stepsTaken = stepsTaken + 1
     return maze
 
+# main
 if __name__ == '__main__':
     maze = make_maze()
     #maze = make_entrance(maze)
@@ -156,3 +172,4 @@ if __name__ == '__main__':
         maze = randomTraverse(maze,seeker)
         print("top = "+str(seeker.top)+"\nbottom = "+str(seeker.bottom)+"\nleft= "+str(seeker.left)+"\nright = "+str(seeker.right)+"\n")
         print(maze)
+    print("Steps Taken = "+str(stepsTaken))
