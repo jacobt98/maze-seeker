73c2a73d7fede28e3a3b6f2b89b8b5009371e295
OhThomas <thomasdonnrader@yahoo.com>
2020-10-28 01:33:39 -0500

    Uniform cost path
    Uniform cost path is now implemented, right now I just have it being fed the real maze string as opposed to the agent just to demonstrate it works. I'm going to implement a traversal function next that have it behave the way we want where the agent doesn't know where the hider is immediately and will use some more visual clues in it to help the agent.

--- ai-project.py
+++ ai-project.py
@@ -1,4 +1,5 @@
 #import msvcrt
+import sys
 import time
 import random
 from random import shuffle, randrange
@@ -14,7 +15,7 @@ slowDown = 0 # used to slow down the simulation
 # Agent class that contains all the information needed for our agents
 class Agent:
     # constructor
-    def __init__(self,name,top,bottom,left,right,currentLocation,opposingAgentLastLocation,spotsVisited,spotsSeen,spotsSeeing,maze):
+    def __init__(self,name,top,bottom,left,right,currentLocation,opposingAgentLastLocation,spotsVisited,spotsSeen,spotsSeeing,path,maze):
         self.name = name
         self.top = top
         self.bottom = bottom
@@ -25,6 +26,7 @@ class Agent:
         self.spotsSeeing = spotsSeeing
         self.spotsVisited = spotsVisited
         self.spotsSeen = spotsSeen
+        self.path = path
         maze = self.fillInBlankMaze(maze)
         self.maze = maze
 
@@ -59,6 +61,31 @@ class Agent:
             i = i + 1
         maze = "".join(mazeList)
         return maze
+
+# Node class that contains information for search algorithms
+class Node:
+    # constructor
+    def __init__(self, location, cost, edges, mazeList):
+        self.location = location
+        self.cost = cost
+        if edges == []:
+            self.edges = self.generateEdges(location,edges,mazeList)
+        else:
+            self.edges = edges
+            
+    def generateEdges(self,location,edges,mazeList):
+        global widthLength
+        if mazeList[location+widthLength] == " " or mazeList[location+widthLength] == "1" or mazeList[location+widthLength] == "0":
+            edges.append(location+widthLength)
+        if mazeList[location-widthLength] == " " or mazeList[location-widthLength] == "1" or mazeList[location-widthLength] == "0":
+            edges.append(location-widthLength)
+        if mazeList[location-1] == " " or mazeList[location-1] == "1" or mazeList[location-1] == "0":
+            edges.append(location-1)
+        if mazeList[location+1] == " " or mazeList[location+1] == "1" or mazeList[location+1] == "0":
+            edges.append(location+1)
+        return edges
+            
+        
 
 # generates a maze
 def make_maze(w = width, h = height):
@@ -390,7 +417,7 @@ def updateLeft(mazeList,agent,currentLocation):
         top = False
     if (mazeList[currentLocation+widthLength] != " " and mazeList[currentLocation+widthLength] != "1" and mazeList[currentLocation+widthLength] != "0") and (mazeList[(currentLocation+(widthLength))-1] != " " and mazeList[(currentLocation+(widthLength))-1] != "1" and mazeList[(currentLocation+(widthLength))-1] != "0"):
         bottom = False
-    while mazeList[i] != "|":
+    while mazeList[i] != "|" and mazeList[i] != "+" and mazeList[i] != "-":
         if mazeList[i] != " " and mazeList[i] != "1" and mazeList[i] != "0":
             break
         agent.addSpotSeeing(mazeList,i)
@@ -403,7 +430,7 @@ def updateLeft(mazeList,agent,currentLocation):
     #agent.addSpotSeen(i) # so further path decision making can be made
     if top == True:
         i = currentLocation - widthLength - 1
-        while mazeList[i] != "|":
+        while mazeList[i] != "|" and mazeList[i] != "+" and mazeList[i] != "-":
             if mazeList[i] != " " and mazeList[i] != "1" and mazeList[i] != "0":
                 break
             agent.addSpotSeeing(mazeList,i)
@@ -415,7 +442,7 @@ def updateLeft(mazeList,agent,currentLocation):
         agent.addSpotSeeing(mazeList,i) # uncomment these when we add array to agent that includes what's actually seen
     if bottom == True:
         i = currentLocation + widthLength - 1
-        while mazeList[i] != "|":
+        while mazeList[i] != "|" and mazeList[i] != "+" and mazeList[i] != "-":
             if mazeList[i] != " " and mazeList[i] != "1" and mazeList[i] != "0":
                 break
             agent.addSpotSeeing(mazeList,i)
@@ -435,7 +462,7 @@ def updateRight(mazeList,agent,currentLocation):
         top = False
     if (mazeList[currentLocation+widthLength] != " " and mazeList[currentLocation+widthLength] != "1" and mazeList[currentLocation+widthLength] != "0") and (mazeList[(currentLocation+(widthLength))+1] != " " and mazeList[(currentLocation+(widthLength))+1] != "1" and mazeList[(currentLocation+(widthLength))+1] != "0"):
         bottom = False
-    while mazeList[i] != "|":
+    while mazeList[i] != "|" and mazeList[i] != "+" and mazeList[i] != "-":
         if mazeList[i] != " " and mazeList[i] != "1" and mazeList[i] != "0":
             break
         agent.addSpotSeeing(mazeList,i)
@@ -448,7 +475,7 @@ def updateRight(mazeList,agent,currentLocation):
     #agent.addSpotSeen(i) # so further path decision making can be made
     if top == True:
         i = currentLocation - widthLength + 1
-        while mazeList[i] != "|":
+        while mazeList[i] != "|"and mazeList[i] != "+" and mazeList[i] != "-":
             if mazeList[i] != " " and mazeList[i] != "1" and mazeList[i] != "0":
                 break
             agent.addSpotSeeing(mazeList,i)
@@ -460,7 +487,7 @@ def updateRight(mazeList,agent,currentLocation):
         agent.addSpotSeeing(mazeList,i) # uncomment these when we add array to agent that includes what's actually seen
     if bottom == True:
         i = currentLocation + widthLength + 1
-        while mazeList[i] != "|":
+        while mazeList[i] != "|" and mazeList[i] != "+" and mazeList[i] != "-":
             if mazeList[i] != " " and mazeList[i] != "1" and mazeList[i] != "0":
                 break
             agent.addSpotSeeing(mazeList,i)
@@ -477,6 +504,79 @@ def updateSight(mazeList,agent,currentLocation):
     updateLeft(mazeList,agent,currentLocation)
     updateRight(mazeList,agent,currentLocation)
 
+# looks through 2 arrays for a certain node and returns true if it's found and replaces it in the array if the cost is less
+def nodeCostCheck(nodesAvailable,nodesVisited,node):
+    for nodeTemp in nodesVisited:
+        if nodeTemp.location == node.location:
+            return nodesAvailable
+    for nodeTemp in nodesAvailable:
+        if nodeTemp.location == node.location:
+            if node.cost < nodeTemp.cost:
+                nodesAvailable.remove(nodeTemp)
+                nodesAvailable.append(node)
+            return nodesAvailable
+    nodesAvailable.append(node)
+    return nodesAvailable
+
+# returns a list of indices in the maze string for a proper path using uniform cost searching algorithms
+def uniformCostPath(mazeList, currentLocation, destination):
+    global widthLength
+    node = Node(currentLocation,0, [], mazeList)
+    nodesVisited = [node]
+    nodesAvailable = []
+    i = currentLocation
+    cost = 0
+    minCost = sys.maxsize
+    pathComplete = 0
+    result = []
+    while pathComplete == 0:
+        cost = cost + 1
+        if widthLength <= i and (mazeList[i-widthLength] == " " or mazeList[i-widthLength] == "1" or mazeList[i-widthLength] == "0"):# widthLength = 50
+            node = Node(i-widthLength, cost, [], mazeList)
+            nodesAvailable = nodeCostCheck(nodesAvailable,nodesVisited,node)
+        if mazeList[i-1] == " " or mazeList[i-1] == "1" or mazeList[i-1] == "0":#i % width != 0 and
+            node = Node(i-1, cost, [], mazeList)
+            nodesAvailable = nodeCostCheck(nodesAvailable,nodesVisited,node)
+        if mazeList[i+1] == " " or mazeList[i+1] == "1" or mazeList[i+1] == "0":#i+1 % width != 0 and
+            node = Node(i+1, cost, [], mazeList)
+            nodesAvailable = nodeCostCheck(nodesAvailable,nodesVisited,node)
+        if i < len(mazeList) - widthLength and (mazeList[i+widthLength] == " " or mazeList[i+widthLength] == "1" or mazeList[i+widthLength] == "0"):
+            node = Node(i+widthLength, cost, [], mazeList)
+            nodesAvailable = nodeCostCheck(nodesAvailable,nodesVisited,node) #if node not in nodesAvailable and nodesVisited else nodesAvailable
+            
+        # Checking if available nodes list is empty
+        if nodesAvailable == []:
+            #print("HERE")
+            pathComplete = -1
+            
+        for node in nodesAvailable:
+            #print("nodes available = "+str(node.location) +" "+str(node.cost))
+            if node.cost < minCost or (node.cost == minCost and node.location == destination):
+                currentNode = node
+                minCost = node.cost
+        # Reach destination
+        if currentNode is not None:
+            #print("yo"+str(currentNode.location))
+            nodesAvailable.remove(currentNode)
+            nodesVisited.append(currentNode)
+            cost = currentNode.cost
+            i = currentNode.location
+            if currentNode.location == destination:
+                pathComplete = 1
+        else:
+            pathComplete = -1
+        minCost = sys.maxsize
+        currentNode = None
+    if pathComplete == 1:
+        result.append(nodesVisited[len(nodesVisited)-1])
+        for node in nodesVisited[::-1]:
+            for edges in result[len(result)-1].edges:
+                if edges == node.location and node.cost < result[len(result)-1].cost:
+                    result.append(node)
+        result = result[::-1]
+        #result = result[1:]
+    return result
+
 # looks for the hider based on what the seeking agent sees (agent.spotsSeeing)
 def checkForHiderSight(mazeList,agent):
     result = 0
@@ -484,6 +584,7 @@ def checkForHiderSight(mazeList,agent):
         if mazeList[i] == "1":
             result = i
             agent.opposingAgentLastLocation = i
+            agent.path = uniformCostPath(list(agent.maze), agent.currentLocation, i)
     return result
 
 # finds the shortest path between the seeker and hider(or last seen) and returns the node that goes towards that path
@@ -582,15 +683,48 @@ def randomTraverseNewSpotsSight(maze, agent):
     updateSight(mazeList,agent,choice) #debugging purposes (so we can see what he's seeing with the print)
     return maze
 
+# Performs a random search until it finds the hiding agent, then it will use A* searching algorithm to find the best path to the hiders last known location    
+def randomTraverseUniformCostSeek(maze, agent, path):
+    global stepsTaken
+    mazeList = list(maze)
+    i = 0
+    choices = 0
+    choicesList = [-1,-1,-1,-1]
+    while i < len(mazeList) and mazeList[i] != agent.name:
+        i = i + 1
+    agent = whatIsAvailable(maze,agent)
+    agent.spotsSeeing.clear()
+    updateSight(mazeList,agent,i)
+    hiderLocation = checkForHiderSight(mazeList,agent)
+    if path != []:
+        for spot in path:
+            if spot.location == i:
+                index = path.index(spot)
+                choice = path[index+1]
+                break
+        choice = choice.location
+        
+    agent.addSpotVisited(choice)
+    mazeList[i] = " "
+    mazeList[choice] = agent.name
+    agent.currentLocation = choice
+    agent = agent_reset(agent)
+    maze = "".join(mazeList)
+    agent = whatIsAvailable(maze,agent)
+    stepsTaken = stepsTaken + 1
+    updateSight(mazeList,agent,choice) #debugging purposes (so we can see what he's seeing with the print)
+    return maze
+
 # main
 if __name__ == '__main__':
     maze = make_maze()
     #maze = make_entrance(maze)
-    seeker = Agent("0",-1,-1,-1,-1,-1,-1,[],[],[],maze) #(name,top,bottom,left,right,currentLocation,opposingAgentLastLocation,spotsVisited,spotsSeen,spotsSeeing,mazeList)
-    hider = Agent("1",-1,-1,-1,-1,-1,-1,[],[],[],maze)
+    seeker = Agent("0",-1,-1,-1,-1,-1,-1,[],[],[],[],maze) #(name,top,bottom,left,right,currentLocation,opposingAgentLastLocation,spotsVisited,spotsSeen,spotsSeeing,path,mazeList)
+    hider = Agent("1",-1,-1,-1,-1,-1,-1,[],[],[],[],maze)
     maze = add_agent(maze, seeker)
     maze = add_agent_random_spot(maze,hider)
     updateSight(list(maze),seeker,seeker.currentLocation)
+    testing = uniformCostPath(list(maze),seeker.currentLocation,hider.currentLocation)
     while gameComplete == 0:
         # Random search
         #maze = randomTraverse(maze,seeker)
@@ -598,8 +732,11 @@ if __name__ == '__main__':
         # Random search to new locations
         #maze = randomTraverseNewSpots(maze,seeker)
 
-        # Random search to new locations with visual clues
-        maze = randomTraverseNewSpotsSight(maze,seeker)
+        # Random search to new locations with visual clues (deprecated, can get caught in a loop with current visual info; creating proper path to the hider with dijkstra/a* algos to fix this)
+        #maze = randomTraverseNewSpotsSight(maze,seeker) 
+
+        # Random search with uniform cost path seeking 
+        maze = randomTraverseUniformCostSeek(maze,seeker,testing) 
 
         # if you press a key it slows down the simulation
         # This only works on Windows        
@@ -615,6 +752,10 @@ if __name__ == '__main__':
         else:
             time.sleep(0.6) # replace with putting this(traverse/keyboardinput)^ in tick from game engine and prints in render(or actual 3d maze)
         
+        #print("SEEKER CURRENT LOCATION = "+str(seeker.currentLocation))
+        #testing = uniformCostPath(list(seeker.maze),seeker.currentLocation,57)
+        #for test in testing:
+        #    print("UNIFORM COST PATH = "+str(test.location))
         print("AGENT MAZE = ")
         print(seeker.maze)
         print("top = "+str(seeker.top)+"\nbottom = "+str(seeker.bottom)+"\nleft= "+str(seeker.left)+"\nright = "+str(seeker.right)+"\n")
